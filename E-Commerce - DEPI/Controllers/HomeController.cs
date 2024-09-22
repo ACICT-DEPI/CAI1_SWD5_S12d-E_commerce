110359949fe04b9a825d44d1143bc701308fda89
marwasameh2003 <marwamostafa322@gmail.com>
2024-09-22 13:17:57 +0300

    added pagination to products

--- E-Commerce - DEPI/Controllers/HomeController.cs
+++ E-Commerce - DEPI/Controllers/HomeController.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using System.IO;
 
-// I NEED TO CHANGE THE WAY I HANDLE DATA TO TAKE IT FROM DATA BASE NOT THOSE IN MEMORY LISTS
 namespace GP.Controllers
 {
     public class HomeController : Controller
@@ -24,21 +23,86 @@ namespace GP.Controllers
 
         public IActionResult Index()
         {
+
             var categories = context.Categories.ToList();
-            //var page = products.Skip(id*6).Take(6).ToList();
-            //ViewBag.next = id+1;
-            //ViewBag.prev = id - 1;
+
             return View(categories);
         }
-        public IActionResult ShowProducts(int id)
+        public IActionResult ShowProducts(int Catid,int pageNo=0)
         {
-            var products = context.Products.Where(x=>x.CatId == id).ToList();
-            return View(products);
+            ViewBag.catid = Catid;
+            ViewBag.next = pageNo + 1;
+            ViewBag.prev = pageNo - 1;
+            if(pageNo<=0)
+            {
+                var page = context.Products.Where(x => x.CatId == Catid).Skip(0 * 6).Take(6).ToList();
+                ViewBag.next = pageNo + 2;
+
+                return View(page);
+
+            }
+            else if(pageNo>=context.Products.Count()/6)
+            {
+                int no = (context.Products.Count() / 6) - 1;
+                var page = context.Products.Where(x => x.CatId == Catid).Skip(no * 6).Take(6).ToList();
+                ViewBag.prev = pageNo - 2;
+
+                return View(page);
+
+            }
+            else
+            {
+                var page = context.Products.Where(x => x.CatId == Catid).Skip(pageNo * 6).Take(6).ToList();
+                return View(page);
+
+            }
+
         }
         public IActionResult ShowDetails(int id)
         {
             Product product = context.Products.Find( id);
             return View(product);
+        }
+        //  ISSUE HERE: need to sort first then paginate , and may combine this action with the show products
+        public IActionResult ShowProductsSorted(string criteria, int id , int Catid, int pageNo = 0)
+        {
+            if(criteria == "Rate")
+            {
+                ViewBag.catid = Catid;
+                ViewBag.next = pageNo + 1;
+                ViewBag.prev = pageNo - 1;
+                var page = context.Products.Where(x => x.CatId == Catid).Skip(pageNo * 6).OrderBy(x => x.Rate).Take(6).ToList();
+
+                return View("ShowProducts", page);
+            }
+            else if (criteria == "Price")
+            {
+                ViewBag.catid = Catid;
+                ViewBag.next = pageNo + 1;
+                ViewBag.prev = pageNo - 1;
+                var page = context.Products.Where(x => x.CatId == Catid).Skip(pageNo * 6).OrderBy(x => x.Price).Take(6).ToList();
+
+                return View("ShowProducts", page);
+            }
+            else if (criteria == "Alpha")
+            {
+                ViewBag.catid = Catid;
+                ViewBag.next = pageNo + 1;
+                ViewBag.prev = pageNo - 1;
+                var page = context.Products.Where(x => x.CatId == Catid).Skip(pageNo * 6).OrderBy(x => x.Name).Take(6).ToList();
+
+                return View("ShowProducts", page);
+            }
+            else
+            {
+                ViewBag.catid = Catid;
+                ViewBag.next = pageNo + 1;
+                ViewBag.prev = pageNo - 1;
+                var page = context.Products.Where(x => x.CatId == Catid).Skip(pageNo * 6).OrderBy(x => x.Rate).Take(6).ToList();
+
+                return View("ShowProducts", page);
+            }
+
         }
         public IActionResult Search(string searchQuery)
         {
